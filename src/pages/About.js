c4c0597f0ee1501dde1907652f8435b81dd463cb
Jean-MarcG <gagnebien.jeanmarc@gmail.com>
2021-10-22 17:04:59 +0200

    update

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navigation from '../componets/Navigation';
+
+const About = () => {
+    return (
+        <div>
+        <Navigation />
+            <h1>À propos</h1>
+            <div className="content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, incidunt suscipit! Nisi voluptatibus perspiciatis eius, sunt veniam eos iusto dolorum, incidunt soluta quod modi nesciunt! Culpa quam inventore facere necessitatibus.
+            </div>
+            <div className="content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, incidunt suscipit! Nisi voluptatibus perspiciatis eius, sunt veniam eos iusto dolorum, incidunt soluta quod modi nesciunt! Culpa quam inventore facere necessitatibus.
+            </div>
+            <div className="content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, incidunt suscipit! Nisi voluptatibus perspiciatis eius, sunt veniam eos iusto dolorum, incidunt soluta quod modi nesciunt! Culpa quam inventore facere necessitatibus.
+            </div>
+            <div className="content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, incidunt suscipit! Nisi voluptatibus perspiciatis eius, sunt veniam eos iusto dolorum, incidunt soluta quod modi nesciunt! Culpa quam inventore facere necessitatibus.
+            </div>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Navigation from "../componets/Navigation";
+
+const Home = () => {
+    return (
+        <div className="main">
+            <Navigation />
+            <h1>Accueil</h1>
+        </div>
+    )
+}
+
+export default Home;
